d2183e9fe94abdb0174fa83a541232a98738ce59
Amal Bansode <52236912+amalbansode@users.noreply.github.com>
2022-03-12 23:37:42 -0500

    Lint library

--- src/numeric_range.hpp
+++ src/numeric_range.hpp
@@ -11,10 +11,10 @@ public:
   double ub = 0;
   bool ub_inclusive = true;
 
-  NumericRange(const double _lb, const bool _lb_inclusive,
-               const double _ub, const bool _ub_inclusive) :
-               lb(_lb), lb_inclusive(_lb_inclusive),
-               ub(_ub), ub_inclusive(_ub_inclusive)
+  NumericRange (const double _lb, const bool _lb_inclusive,
+                const double _ub, const bool _ub_inclusive) :
+      lb(_lb), lb_inclusive(_lb_inclusive),
+      ub(_ub), ub_inclusive(_ub_inclusive)
   {
     if (lb > ub)
     {
@@ -26,9 +26,9 @@ public:
     }
   }
 
-  explicit NumericRange(double _scalar) :
-                        lb(_scalar), lb_inclusive(true),
-                        ub(_scalar), ub_inclusive(true)
+  explicit NumericRange (double _scalar) :
+      lb(_scalar), lb_inclusive(true),
+      ub(_scalar), ub_inclusive(true)
   {}
 };
 
@@ -36,7 +36,7 @@ public:
 class NumericRangeComparator
 {
 public:
-  bool operator()(const NumericRange& lhs, const NumericRange& rhs) const
+  bool operator() (const NumericRange &lhs, const NumericRange &rhs) const
   {
     const bool lhs_is_scalar = (lhs.lb == lhs.ub);
     const bool rhs_is_scalar = (rhs.lb == rhs.ub);
@@ -56,21 +56,24 @@ public:
     else
     {
       if (lhs.lb == rhs.lb && lhs.lb_inclusive == rhs.lb_inclusive
-       && lhs.ub == rhs.ub && lhs.ub_inclusive == rhs.ub_inclusive)
+          && lhs.ub == rhs.ub && lhs.ub_inclusive == rhs.ub_inclusive)
       {
         return false;
       }
-      else if ((lhs.ub < rhs.lb) || (lhs.ub == rhs.lb && !(lhs.ub_inclusive && rhs.lb_inclusive)))
+      else if ((lhs.ub < rhs.lb) ||
+               (lhs.ub == rhs.lb && !(lhs.ub_inclusive && rhs.lb_inclusive)))
       {
         return true;
       }
-      else if ((lhs.lb > rhs.ub) || (lhs.lb == rhs.ub && !(lhs.lb_inclusive && rhs.ub_inclusive)))
+      else if ((lhs.lb > rhs.ub) ||
+               (lhs.lb == rhs.ub && !(lhs.lb_inclusive && rhs.ub_inclusive)))
       {
         return false;
       }
       else
       {
-        throw std::runtime_error("Invalid comparison between overlapping ranges");
+        throw std::runtime_error(
+            "Invalid comparison between overlapping ranges");
       }
     }
   } // bool operator()
