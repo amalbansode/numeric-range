f2b6dedb5dd8c556235e79c1f73ea88a3cec799f
Amal Bansode <52236912+amalbansode@users.noreply.github.com>
2022-03-12 22:28:54 -0500

    First verbose attempt, passes all tests

---
+++ src/numeric_range.h
@@ -0,0 +1,94 @@
+#ifndef JSON_EQUATION_JSON_EQUATION_H
+#define JSON_EQUATION_JSON_EQUATION_H
+
+#include <exception>
+
+class NumericRange
+{
+public:
+  double lb = 0;
+  bool lb_inclusive = true;
+  double ub = 0;
+  bool ub_inclusive = true;
+
+  NumericRange(const double _lb, const bool _lb_inclusive,
+               const double _ub, const bool _ub_inclusive) :
+               lb(_lb), lb_inclusive(_lb_inclusive),
+               ub(_ub), ub_inclusive(_ub_inclusive)
+  {}
+};
+
+// Is LHS < RHS?
+class NumericRangeComparator
+{
+public:
+  bool operator()(const NumericRange& lhs, const NumericRange& rhs)
+  {
+    const bool lhs_is_scalar = (lhs.lb == lhs.ub);
+    const bool rhs_is_scalar = (rhs.lb == rhs.ub);
+
+    if (lhs_is_scalar && rhs_is_scalar)
+    {
+      if (lhs.lb < rhs.lb)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else if (lhs_is_scalar)
+    {
+      if ((lhs.lb < rhs.lb && rhs.lb_inclusive) || (lhs.lb == rhs.lb && !rhs.lb_inclusive))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else if (rhs_is_scalar)
+    {
+      if ((lhs.ub < rhs.lb && lhs.ub_inclusive) || (lhs.ub == rhs.lb && !lhs.ub_inclusive))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else
+    {
+      if (lhs.lb == rhs.lb && lhs.lb_inclusive == rhs.lb_inclusive
+       && lhs.ub == rhs.ub && lhs.ub_inclusive == rhs.ub_inclusive)
+      {
+        return false;
+      }
+      else if (lhs.ub < rhs.lb)
+      {
+        return true;
+      }
+      else if (lhs.ub == rhs.lb && !(lhs.ub_inclusive && rhs.lb_inclusive))
+      {
+        return true;
+      }
+      else if (lhs.lb > rhs.ub)
+      {
+        return false;
+      }
+      else if (lhs.lb == rhs.ub && !(lhs.lb_inclusive && rhs.ub_inclusive))
+      {
+        return false;
+      }
+      else
+      {
+        throw std::runtime_error("Invalid comparison between overlapping ranges");
+      }
+    }
+  } // bool operator()
+}; // class NumericRangeComparator
+
+#endif //JSON_EQUATION_JSON_EQUATION_H
